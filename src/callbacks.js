6ea9dc71f242a63b25eb1eab9230462dbc1207ac
revaxl <robin208@hotmail.fr>
2021-05-06 01:03:22 +0900

    override route method to reset PMC HP to default values on logout

--- src/callbacks.js
+++ src/callbacks.js
@@ -5,22 +5,38 @@ class Callbacks {
     constructor() {
         // override a route that does nothing meaningful in the game
         HttpRouter.onStaticRoute["/client/game/start"].aki = this.makePmcStrong.bind(this);
+        HttpRouter.onStaticRoute["/client/game/logout"].aki = this.resetPMCHealth.bind(this);
+        this.config = require('../config.json');
     }
 
     makePmcStrong(url, info, sessionID) {
-        const config = require('../config.json');
-        // get the PMC super body parts
-        const bodyParts = config.globals_config.Health.bodyParts; 
-
-        if (config.globals_config.Health.super === true) { // check if the config for super pmc is enabled
+        if (this.config.globals_config.Health.super === true) { // check if the config for super pmc is enabled
+            // get the PMC super body parts only if the super option is enabled so we don't waste time
+            const bodyParts = this.config.globals_config.Health.bodyParts; 
             const pmc = ProfileController.getPmcProfile(sessionID); // get the current logged in player's profile
             const pmcBodyParts = pmc.Health.BodyParts; // get the current player body parts
             for (let bodyPart in pmcBodyParts) { // loop through the parts and update the value to the super ones
-                pmcBody[bodyPart].Health.Maximum = bodyParts[bodyPart];
-                pmcBody[bodyPart].Health.Current = bodyParts[bodyPart];
+                pmcBodyParts[bodyPart].Health.Maximum = bodyParts[bodyPart].new;
+                pmcBodyParts[bodyPart].Health.Current = bodyParts[bodyPart].new;
             }
         }
 
+        // return the original response
+        return HttpResponse.nullResponse();
+    }
+
+    resetPMCHealth(url, info, sessionID) {
+        if (this.config.globals_config.Health.super === true) {
+            const bodyParts = this.config.globals_config.Health.bodyParts; 
+            const pmc = ProfileController.getPmcProfile(sessionID);
+            const pmcBodyParts = pmc.Health.BodyParts;
+
+            for (let bodyPart in pmcBodyParts) {
+                pmcBodyParts[bodyPart].Health.Maximum = bodyParts[bodyPart].default;
+                pmcBodyParts[bodyPart].Health.Current = bodyParts[bodyPart].default;
+            }
+        }
+        
         // return the original response
         return HttpResponse.nullResponse();
     }
